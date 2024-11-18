4dcd83d8be59b7ad3a98705519d7232ac5cbdb79
Ekaterina Levkova <mishinacaterina@yandex.ru>
2024-11-18 20:09:01 +0300

    adding comments

--- OOP1.py
+++ OOP1.py
@@ -10,6 +10,7 @@ class Student:
     def add_courses(self, course):
         self.courses_in_progress.append(course)
 
+    "Позволяет студенту выставлять оценки лектору"
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
             if course in lecturer.grades:
@@ -26,8 +27,10 @@ class Student:
         return sum(total_grades) / len(total_grades) if total_grades else 0
 
     def __str__(self):
+        "Возвращает строку с именем, фамилией, средней оценкой за ДЗ, текущими и завершенными курсами"
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average_grade():.1f}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
 
+    "Используются методы __lt__ и __eq__ для сравнения студентов по средней оценке за ДЗ"
     def __lt__(self, other):
         if not isinstance(other, Student):
             raise TypeError("Можно сравнивать только объекты типа Student")
@@ -58,6 +61,7 @@ class Lecturer(Mentor):
         else:
             return 'Ошибка'
 
+    "Подсчёт среднего балла на основе полученных оценок от студентов"
     def average_grade(self):
         total_grades = []
         for grades in self.grades.values():
@@ -65,8 +69,10 @@ class Lecturer(Mentor):
         return sum(total_grades) / len(total_grades) if total_grades else 0
 
     def __str__(self):
+        "Возвращает строку с именем, фамилией и средней оценкой за лекции"
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grade():.1f}'
 
+    "Используются методы __lt__ и __eq__ для сравнения лекторов по средней оценке за лекции"
     def __lt__(self, other):
         if not isinstance(other, Lecturer):
             raise TypeError("Можно сравнивать только объекты типа Lecturer")
@@ -81,6 +87,7 @@ class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
+    "Выставление оценки студенту за выполнение ДЗ"
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -91,6 +98,7 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
     def __str__(self):
+        "Возвращает строку с именем и фамилией"
         return f'Имя: {self.name}\nФамилия: {self.surname}'
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
