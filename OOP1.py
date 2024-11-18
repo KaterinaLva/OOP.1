84aa6eee7942471dd094f8e70bf34a8fe4ceaf2d
Ekaterina Levkova <mishinacaterina@yandex.ru>
2024-11-18 21:09:05 +0300

    comments

--- OOP1.py
+++ OOP1.py
@@ -100,15 +100,101 @@ class Reviewer(Mentor):
     def __str__(self):
         "Возвращает строку с именем и фамилией"
         return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+def average_hw_grade(students, course):
+    """Подсчет средней оценки за домашние задания по всем студентам в рамках конкретного курса."""
+    total_grades = []
+    for student in students:
+        if course in student.grades:
+            total_grades.extend(student.grades[course])
+    return sum(total_grades) / len(total_grades) if total_grades else 0
+
+
+def average_lectures_grade(lectors, course):
+    """Подсчет средней оценки за лекции всех лекторов в рамках курса."""
+    total_grades = []
+    for lector in lectors:
+        if course in lector.grades:
+            total_grades.extend(lector.grades[course])
+    return sum(total_grades) / len(total_grades) if total_grades else 0
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+# Создаем студентов
+student1 = Student('Степан', 'Леднёв', 'Мужской')
+student1.add_courses('Python')
+student1.add_courses('ООП')
+
+student2 = Student('Анна', 'Петрова', 'Женский')
+student2.add_courses('C++')
+student2.finished_courses.append('Введение в программирование')
+
+# Создаем лекторов и закрепляем их за курсом
+lecturer1 = Lecturer('Нестор', 'Северов')
+lecturer1.courses_attached.append('Python')
+
+lecturer2 = Lecturer('Ираида', 'Васильева')
+lecturer2.courses_attached.append('С++')
+
+# Создаем экспертов
+reviewer1 = Reviewer('Иван', 'Сидорова')
+reviewer1.courses_attached.append('Python')
+
+reviewer2 = Reviewer('Ольга', 'Иванова')
+reviewer2.courses_attached.append('С++')
+
+# Выставление оценок студентам
+lecturer1.rate_student(student1, 'Python', 9)
+lecturer1.rate_student(student1, 'Python', 8)
+lecturer1.rate_student(student1, 'Python', 7)
+
+lecturer2.rate_student(student2, 'С++', 6)
+lecturer2.rate_student(student2, 'С++', 7)
+lecturer2.rate_student(student2, 'С++', 8)
+
+# Проверка домашних заданий студентов
+reviewer1.rate_hw(student1, 'Python', 10)
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student1, 'Python', 8)
+
+reviewer2.rate_hw(student2, 'С++', 9)
+reviewer2.rate_hw(student2, 'С++', 8)
+reviewer2.rate_hw(student2, 'С++', 7)
+
+# Выставление оценок лекторам
+student1.rate_lecturer(lecturer1, 'Python', 9)
+student1.rate_lecturer(lecturer1, 'Python', 8)
+student1.rate_lecturer(lecturer1, 'Python', 7)
+
+student2.rate_lecturer(lecturer2, 'С++', 6)
+student2.rate_lecturer(lecturer2, 'С++', 7)
+student2.rate_lecturer(lecturer2, 'С++', 8)
+
+# Вывод информации об объектах
+print(student1)
+print(student2)
+print(lecturer1)
+print(lecturer2)
+print(reviewer1)
+print(reviewer2)
+
+# Сравнение студентов и лекторов
+print("Сравниваем студентов:")
+print(student1 > student2)
+print()
+
+print("\nСравниваем лекторов:")
+print(lecturer1 > lecturer2)
+print()
+
+# Подсчет средней оценки за домашние задания по всем студентам в рамках курса
+students_list = [student1, student2]
+course = 'Python'
+avg_hw_grade = average_hw_grade(students_list, course)
+print(f"\nСредняя оценка за домашние задания по курсу '{course}': {avg_hw_grade:.1f}")
+print()
+
+# Подсчет средней оценки за лекции всех лекторов в рамках курса
+lectors_list = [lecturer1, lecturer2]
+course_lecturers = 'С++'
+avg_lectures_grade = average_lectures_grade(lectors_list, course_lecturers)
+print(f"Средняя оценка за лекции по курсу '{course_lecturers}': {avg_lectures_grade:.1f}")
+print()
